e67fe767f916d35e9508ae8ba57667b83b5a1d9d
btkira <btr_manuel2@hotmail.com>
2016-11-25 19:29:35 -0500

    css login
    de

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,18 @@
     <title>Sistema</title>
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="{{asset('panel/bootstrap/css/bootstrap.min.css')}}" rel='stylesheet' type='text/css'>
+    <link href="{{asset('panel/font-awesome/css/font-awesome.min.css')}}" rel='stylesheet' type='text/css'>
+    
+    <link href="{{asset('auth/css/style.css')}}" rel='stylesheet' type='text/css'>
+
 
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
     
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    
 
     
 </head>
